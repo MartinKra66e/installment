b1f76dfc2f800210d854426724c1e2f631b1a715
Martin Krabbe <159114715+MartinKra66e@users.noreply.github.com>
2024-03-15 11:06:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo apt update -y
 sudo apt upgrade -y
 
-cho "installing Sublime Text"
+echo "installing Sublime Text"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update -y
@@ -39,3 +39,6 @@ echo "installing golang"
 sudo apt install golang-go -y
 echo "running nxc for the first time"
 nxc --version
+echo "installing uploadserver"
+python3 -m pip install --user uploadserver
+
